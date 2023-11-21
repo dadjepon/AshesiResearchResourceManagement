d2683dcd6735fed770d7aad47c66e7e484dcf899
Richard Kweenu Nana Quayson <richard.quayson@ashesi.edu.gh>
2023-11-21 16:53:34 +0000

    proof of concept tests

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-def read_country_file():
+def read_country_file(filename):
     """
     
     """
 
     COUNTRY_CODES = dict()
-    country_file = open("country-codes.csv", "r")
+    country_file = open(filename, "r")
 
     # skip the first line (headers)
     country_file.readline()
@@ -27,3 +27,36 @@ def read_country_file():
 
     country_file.close()
     return COUNTRY_CODES
+
+
+def read_user_data(filename):
+
+    with open(filename, "r") as user_data_file:
+        user_data_file.readline()       # skip the headers
+        user_data = user_data_file.read().splitlines()
+        user_data_file.close()
+    
+    return user_data
+
+
+def build_account_dict(user_info):
+    
+    user_info = user_info.split(",")
+    user_account_details = dict()
+    user_account_details["employee_id"] = user_info[0].strip()
+    user_account_details["firstname"] = user_info[1].strip()
+    user_account_details["lastname"] = user_info[2].strip()
+    user_account_details["email"] = user_info[3].strip()
+    user_account_details["mobile_number"] = user_info[4].strip()
+    user_account_details["role"] = user_info[5].strip()
+    user_account_details["nationality"] = user_info[6].strip()
+
+    return user_account_details
+
+
+if __name__ == "__main__":
+    country_codes = read_country_file("country-codes.csv")
+    user_account_info = read_user_data("users_data.csv")
+    
+    for user_info in user_account_info:
+        print(build_account_dict(user_info))
