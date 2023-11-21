08394b9588c66cd471048293aa2609f52604e5ee
Richard Kweenu Nana Quayson <richard.quayson@ashesi.edu.gh>
2023-11-21 16:51:13 +0000

    Added helper methods to premake accounts

--- Backend/ARRM/Account/helper.py
+++ Backend/ARRM/Account/helper.py
@@ -1,18 +1,18 @@
-def read_country_file():
+import json
+from operator import index
+
+
+def read_country_file(filename):
     """
-    
+    COUNTRY FILE COLUMNS:
+        COUNTRY_NAME, COUNTRY_CODE, INT_DIALING
     """
 
     COUNTRY_CODES = dict()
-    country_file = open("country-codes.csv", "r")
+    country_file = open(filename, "r")
 
     # skip the first line (headers)
     country_file.readline()
-
-    """
-    COUNTRY FILE COLUMNS:
-    COUNTRY_NAME, COUNTRY_CODE, INT_DIALING
-    """
 
     COUNTRY_NAME_INDEX = 0
     INT_DIALING_INDEX = 1
@@ -27,3 +27,28 @@ def read_country_file():
 
     country_file.close()
     return COUNTRY_CODES
+
+
+def read_user_data(filename):
+
+    with open(filename, "r") as user_data_file:
+        user_data_file.readline()       # skip the headers
+        user_data = user_data_file.read().splitlines()
+        user_data_file.close()
+    
+    return user_data
+
+
+def build_account_dict(user_info):
+
+    user_info = user_info.split(",")
+    user_account_details = dict()
+    user_account_details["employee_id"] = user_info[0].strip()
+    user_account_details["firstname"] = user_info[1].strip()
+    user_account_details["lastname"] = user_info[2].strip()
+    user_account_details["email"] = user_info[3].strip()
+    user_account_details["mobile_number"] = user_info[4].strip()
+    user_account_details["role"] = user_info[5].strip()
+    user_account_details["nationality"] = user_info[6].strip()
+
+    return user_account_details
