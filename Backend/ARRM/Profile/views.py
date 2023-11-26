18768b864bc966434d0ffb779aba3b9fd6bcb60b
Richard Kweenu Nana Quayson <richard.quayson@ashesi.edu.gh>
2023-11-26 16:09:40 +0000

    Added enpoints for CRUD features of WritingSample model

--- Backend/ARRM/Profile/views.py
+++ Backend/ARRM/Profile/views.py
@@ -4,8 +4,8 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 import os
 
-from .models import Degree
-from .serializers import DegreeSerializer
+from .models import Degree, WritingSample
+from .serializers import DegreeSerializer, WritingSampleSerializer
 from Account.models import UserAccount
 from Account.permissions import IsBlacklistedToken
 
@@ -126,3 +126,91 @@ class DeleteDegreePermanentlyView(APIView):
             degree.transcript.delete(save=True)
 
         return Response({"success": "Degree deleted successfully"}, status=status.HTTP_200_OK)
+
+
+class AddWritingSampleView(APIView):
+    permission_classes = [IsAuthenticated, IsBlacklistedToken]
+
+    def post(self, request):
+        serializer = WritingSampleSerializer(data=request.data, context={"request": request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class RetrieveWritingSampleView(APIView):
+    permission_classes = [IsAuthenticated, IsBlacklistedToken]
+
+    def get(self, request, sample_id):
+        try:
+            sample = WritingSample.objects.get(id=sample_id)
+        except WritingSample.DoesNotExist:
+            return Response({"error": "Writing sample not found!"}, status=status.HTTP_404_NOT_FOUND)
+        
+        if sample.user != request.user:
+            return Response({"error": "You do not have permission for this resource!"}, status=status.HTTP_403_FORBIDDEN)
+        
+        serializer = WritingSampleSerializer(sample, context={"request": request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+class RetrieveWritingSamplesView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated, IsBlacklistedToken]
+    serializer_class = WritingSampleSerializer
+    queryset = WritingSample.objects.all()
+    filterset_fields = ["title"]
+
+    def get_queryset(self):
+        return WritingSample.objects.filter(user=self.request.user)
+    
+
+class UpdateWritingSampleView(APIView):
+    permission_classes = [IsAuthenticated, IsBlacklistedToken]
+
+    def patch(self, request, sample_id):
+        try:
+            sample = WritingSample.objects.get(id=sample_id)
+        except WritingSample.DoesNotExist:
+            return Response({"error": "Writing sample not found!"}, status=status.HTTP_404_NOT_FOUND)
+        
+        if sample.user != request.user:
+            return Response({"error": "You do not have permission for this resource!"}, status=status.HTTP_403_FORBIDDEN)
+        
+        serializer = WritingSampleSerializer(sample, data=request.data, partial=True, context={"request": request})
+        if serializer.is_valid():
+            # if a new sample is uploaded, delete the old one
+            if "sample" in request.data and sample.sample:
+                if os.path.exists(sample.sample.path):
+                    os.remove(sample.sample.path)
+
+                sample.sample.delete(save=True)
+
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class DeleteWritingSampleView(APIView):
+    permission_classes = [IsAuthenticated, IsBlacklistedToken]
+
+    def delete(self, request, sample_id):
+        try:
+            sample = WritingSample.objects.get(id=sample_id)
+        except WritingSample.DoesNotExist:
+            return Response({"error": "Writing sample not found!"}, status=status.HTTP_404_NOT_FOUND)
+        
+        if sample.user != request.user:
+            return Response({"error": "You do not have permission for this resource!"}, status=status.HTTP_403_FORBIDDEN)
+        
+        sample.delete()
+        
+        if os.path.exists(sample.sample.path):
+            os.remove(sample.sample.path)
+
+        if sample.sample:
+            sample.sample.delete(save=True)
+
+        return Response({"success": "Writing sample deleted successfully"}, status=status.HTTP_200_OK)
