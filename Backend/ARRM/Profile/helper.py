9938bf988b72d5eeb776c79586a6b90434cb4043
Richard Kweenu Nana Quayson <richard.quayson@ashesi.edu.gh>
2023-11-26 16:07:00 +0000

    Added helper method for writing sample path

--- Backend/ARRM/Profile/helper.py
+++ Backend/ARRM/Profile/helper.py
@@ -12,3 +12,19 @@ def transcript_upload_path(instance, filename):
 
     username = instance.user.email.split("@")[0]
     return f"transcripts/{username}/{filename}"
+
+
+def sample_upload_path(instance, filename):
+    """
+    defines upload path for writing sample
+
+    Args:
+        - instance: instance of writing sample model
+        - filename: name of file
+
+    Returns:
+        - path: path to upload writing sample
+    """
+
+    username = instance.user.email.split("@")[0]
+    return f"samples/{username}/{filename}"
