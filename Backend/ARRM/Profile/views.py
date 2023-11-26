c5433168711410f32c08204be512e7388519b2e4
Richard Kweenu Nana Quayson <richard.quayson@ashesi.edu.gh>
2023-11-26 04:33:01 +0000

    Defined endpoints for Degree model

---
+++ Backend/ARRM/Profile/views.py
@@ -0,0 +1,128 @@
+from rest_framework.views import APIView
+from rest_framework import status, generics
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+import os
+
+from .models import Degree
+from .serializers import DegreeSerializer
+from Account.models import UserAccount
+from Account.permissions import IsBlacklistedToken
+
+
+class AddDegreeView(APIView):
+    permission_classes = [IsAuthenticated, IsBlacklistedToken]
+
+    def post(self, request):
+        serializer = DegreeSerializer(data=request.data, context={"request": request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class RetrieveDegreeView(APIView):
+    permission_classes = [IsAuthenticated, IsBlacklistedToken]
+
+    def get(self, request, degree_id):
+        try:
+            degree = Degree.objects.get(id=degree_id)
+        except Degree.DoesNotExist:
+            return Response({"error": "Degree not found!"}, status=status.HTTP_404_NOT_FOUND)
+        
+        if degree.user != request.user:
+            return Response({"error": "You do not have permission for this resource!"}, status=status.HTTP_403_FORBIDDEN)
+        
+        serializer = DegreeSerializer(degree, context={"request": request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+class RetrieveDegreesView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated, IsBlacklistedToken]
+    serializer_class = DegreeSerializer
+    queryset = Degree.objects.all()
+    filterset_fields = ["type", "university", "major", "graduation_year"]
+
+    def get_queryset(self):
+        return Degree.objects.filter(user=self.request.user, is_deleted=False)
+    
+
+class UpdateDegreeView(APIView):
+    permission_classes = [IsAuthenticated, IsBlacklistedToken]
+
+    def patch(self, request, degree_id):
+        try:
+            degree = Degree.objects.get(id=degree_id)
+        except Degree.DoesNotExist:
+            return Response({"error": "Degree not found!"}, status=status.HTTP_404_NOT_FOUND)
+        
+        if degree.user != request.user:
+            return Response({"error": "You do not have permission for this resource!"}, status=status.HTTP_403_FORBIDDEN)
+        
+        serializer = DegreeSerializer(degree, data=request.data, partial=True, context={"request": request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class DeleteDegreeView(APIView):
+    permission_classes = [IsAuthenticated, IsBlacklistedToken]
+
+    def patch(self, request, degree_id):
+        try:
+            degree = Degree.objects.get(id=degree_id)
+        except Degree.DoesNotExist:
+            return Response({"error": "Degree not found!"}, status=status.HTTP_404_NOT_FOUND)
+        
+        if degree.user != request.user:
+            return Response({"error": "You do not have permission for this resource!"}, status=status.HTTP_403_FORBIDDEN)
+        
+        degree.is_deleted = True
+        degree.save()
+        return Response({"success": "Degree deleted successfully"}, status=status.HTTP_200_OK)
+    
+
+class RestoreDegreeView(APIView):
+    permission_classes = [IsAuthenticated, IsBlacklistedToken]
+
+    def patch(self, request, degree_id):
+        try:
+            degree = Degree.objects.get(id=degree_id)
+        except Degree.DoesNotExist:
+            return Response({"error": "Degree not found!"}, status=status.HTTP_404_NOT_FOUND)
+        
+        if degree.user != request.user:
+            return Response({"error": "You do not have permission for this resource!"}, status=status.HTTP_403_FORBIDDEN)
+        
+        degree.is_deleted = False
+        degree.save()
+        return Response({"success": "Degree restored successfully"}, status=status.HTTP_200_OK)
+
+
+class DeleteDegreePermanentlyView(APIView):
+    permission_classes = [IsAuthenticated, IsBlacklistedToken]
+
+    def delete(self, request, degree_id):
+        try:
+            degree = Degree.objects.get(id=degree_id)
+        except Degree.DoesNotExist:
+            return Response({"error": "Degree not found!"}, status=status.HTTP_404_NOT_FOUND)
+        
+        if degree.user != request.user:
+            return Response({"error": "You do not have permission for this resource!"}, status=status.HTTP_403_FORBIDDEN)
+        
+        if not degree.is_deleted:
+            return Response({"error": "Degree is not in trash!"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        degree.delete()
+        
+        if os.path.exists(degree.transcript.path):
+            os.remove(degree.transcript.path)
+
+        if degree.transcript:
+            degree.transcript.delete(save=True)
+
+        return Response({"success": "Degree deleted successfully"}, status=status.HTTP_200_OK)
