e5f8974dd5c919f38a974c6d2e221c09562ec261
Richard Kweenu Nana Quayson <richard.quayson@ashesi.edu.gh>
2023-11-29 00:19:47 +0000

    added models for RA and Faculty profile

--- Backend/ARRM/Profile/models.py
+++ Backend/ARRM/Profile/models.py
@@ -4,7 +4,8 @@ from django.core.validators import MaxValueValidator
 from datetime import datetime
 
 from Account.models import UserAccount
-from .helper import transcript_upload_path, sample_upload_path
+from .helper import (transcript_upload_path, sample_upload_path, profile_picture_upload_path,
+                     cv_upload_path)
 
 
 class DegreeType(models.TextChoices):
@@ -43,13 +44,13 @@ class Degree(models.Model):
     defines attributes for a degree class
 
     Attributes:
-        - user: user's account
-        - type: degree type (BSc, MSc, PhD)
-        - university: name of university
-        - major: major of degree
-        - graduation_year: year of graduation
-        - transcript: degree transcript
-        - created_at: date degree was created
+        - user (UserAccount): the user's account
+        - type (CharField): type of degree
+        - university (CharField): university of degree
+        - major (CharField): major of degree
+        - graduation_year (IntegerField): year of graduation
+        - transcript (FileField): degree transcript
+        - created_at (DateTimeField): date degree was created
     """
 
     user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
@@ -63,7 +64,7 @@ class Degree(models.Model):
     is_verified = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.type
+        return f"{self.type} in {self.major} from {self.university} - {self.graduation_year}"
 
 
 class WritingSample(models.Model):
@@ -71,11 +72,11 @@ class WritingSample(models.Model):
     defines attributes for a writing sample class
 
     Attributes:
-        - user: user's account
-        - title: title of writing sample
-        - publication_link: link to publication
-        - sample: writing sample
-        - created_at: date writing sample was created
+        - user (UserAccount): the user's account
+        - title (CharField): title of writing sample
+        - publication_link (CharField): link to publication
+        - sample (FileField): writing sample
+        - created_at (DateTimeField): date writing sample was created
     """
 
     user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
@@ -86,3 +87,65 @@ class WritingSample(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Interest(models.Model):
+    """
+    defines attributes for an interest class
+
+    Attributes:
+        - name: name of interest
+        - study_area: study area of interest
+    """
+
+    name = models.CharField(max_length=100, unique=True)
+    study_area = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.name} : {self.study_area}"
+
+
+class ResearchAssistant(models.Model):
+    """
+    defines attributes for a research assistant class
+
+    Attributes:
+        - user (UserAccount): the user's account
+        - bio (TextField): the user's bio
+        - profile_picture (ImageField): the user's profile picture
+        - interests (ManyToManyField): the user's interests
+        - linkedin_url (CharField): the user's linkedin url
+        - cv (FileField): the user's cv
+    """
+
+    user = models.OneToOneField(UserAccount, on_delete=models.CASCADE, primary_key=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to=profile_picture_upload_path, blank=True, null=True)
+    interests = models.ManyToManyField(Interest, blank=True, null=True)
+    linkedin_url = models.CharField(max_length=250, blank=True, null=True)
+    cv = models.FileField(upload_to=cv_upload_path, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.firstname} {self.user.lastname} - {self.linkedin_url}"
+    
+
+class Faculty(models.Model):
+    """
+    defines attributes for a faculty class
+
+    Attributes:
+        - user (UserAccount): the user's account
+        - bio (TextField): the user's bio
+        - profile_picture (ImageField): the user's profile picture
+        - department (CharField): the user's department
+        - interests (ManyToManyField): the user's interests
+    """
+
+    user = models.OneToOneField(UserAccount, on_delete=models.CASCADE, primary_key=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to=profile_picture_upload_path, blank=True, null=True)
+    department = models.CharField(max_length=100, blank=True, null=True)
+    interests = models.ManyToManyField(Interest, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.firstname} {self.user.lastname} - {self.department}"
