d011a651a02be61404c98023f07cae307a0b6e24
Richard Kweenu Nana Quayson <richard.quayson@ashesi.edu.gh>
2023-12-04 04:45:11 +0000

    configured endpoint for changing project visibility

--- Backend/ARRM/Project/urls.py
+++ Backend/ARRM/Project/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import (
     AddProjectView, RetrieveProjectView, RetrieveProjectsView, UpdateProjectView,
-    RemoveProjectStudyAreaView, DeleteProjectView, RestoreProjectView, 
-    DeleteProjectPermanentlyView,
+    RemoveProjectStudyAreaView, ChangeProjectVisibilityView, DeleteProjectView, 
+    RestoreProjectView, DeleteProjectPermanentlyView,
 )
 
 urlpatterns = [
@@ -11,7 +11,8 @@ urlpatterns = [
     path("get/<int:project_id>/", RetrieveProjectView.as_view(), name="retrieve-project"),
     path("get/", RetrieveProjectsView.as_view(), name="retrieve-projects"),
     path("update/<int:project_id>/", UpdateProjectView.as_view(), name="update-project"),
-    path("study_area/remove/<int:study_area_id>/", RemoveProjectStudyAreaView.as_view(), name="remove-project-study-area"),
+    path("study_area/remove/", RemoveProjectStudyAreaView.as_view(), name="remove-project-study-area"),
+    path("visibility/change/<int:project_id>/", ChangeProjectVisibilityView.as_view(), name="change-project-visibility"),
     path("delete/<int:project_id>/", DeleteProjectView.as_view(), name="delete-project"),
     path("restore/<int:project_id>/", RestoreProjectView.as_view(), name="restore-project"),
     path("delete/permanently/<int:project_id>/", DeleteProjectPermanentlyView.as_view(), name="delete-project-permanently"),
