022d2ed51b9eac4729ec389f6dfe620f68105cc2
Richard Kweenu Nana Quayson <richard.quayson@ashesi.edu.gh>
2023-12-04 22:51:03 +0000

    added helper dict for milestone and task creation

---
+++ Backend/ARRM/Project/helper.py
@@ -0,0 +1,39 @@
+MILESTONE_DICT = {
+    "project_planning": "Project Planning",
+    "literature_review": "Literature Review",
+    "data_collection_and_processing": "Data Collection and Processing",
+    "dissemination": "Dissemination"
+}
+
+PROJECT_MILESTONE_TEMPLATE_DICT = {
+    "project_planning": [
+        "Admin & Project Management",
+        "Reporting",
+        "IRB application"
+    ],
+
+    "literature_review": [
+        "Literature review"
+    ],
+
+    "data_collection_and_processing": [
+        "Development of interview guide for Employers",
+        "Development of data collection instruments",
+        "Finalisation of research instrument",
+        "Review of Instrument",
+        "Piloting of instruments",
+        "Post-Pilot Update of instruments",
+        "Administration of instruments",
+        "Data cleaning and processing",
+        "Data analysis"
+    ],
+
+    "dissemination": [
+        "Manuscript development",
+        "Pitch design services",
+        "Presentation preparation for conference",
+        "Manuscript proofreading services",
+        "Review of Manuscript",
+        "Publication of manuscript"
+    ]
+}
