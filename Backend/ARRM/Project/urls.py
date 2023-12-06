9a8ac859574536883cb3693e0d6c5a1f8b77b5ae
Richard Kweenu Nana Quayson <richard.quayson@ashesi.edu.gh>
2023-12-06 01:06:35 +0000

    configured routes for project team, requests and invitation models

--- Backend/ARRM/Project/urls.py
+++ Backend/ARRM/Project/urls.py
@@ -4,6 +4,14 @@ from .views import (
     RemoveProjectStudyAreaView, ChangeProjectVisibilityView, DeleteProjectView, 
     RestoreProjectView, DeleteProjectPermanentlyView,
 
+    RequestProjectMembershipView, RetrieveProjectMembershipRequestsView, 
+    AcceptProjectMembershipView, RejectProjectMembershipView, DeleteProjectMembershipRequestView,
+
+    InviteResearchAssistantView, RetrieveProjectInvitationsView, AcceptProjectInvitationView,
+    DeclineProjectInvitationView, DeleteProjectInvitationView, 
+    
+    RetrieveProjectTeamMembersView, RemoveProjectTeamMemberView,
+
     AddMilestoneView, RetrieveMilestoneView, RetrieveMilestonesView, DeleteMilestoneView,
 
     RetrieveProjectMilestoneView, RemoveProjectTaskFromMilestoneView, DeleteProjectMilestoneView,
@@ -22,6 +30,24 @@ urlpatterns = [
     path("delete/<int:project_id>/", DeleteProjectView.as_view(), name="delete-project"),
     path("restore/<int:project_id>/", RestoreProjectView.as_view(), name="restore-project"),
     path("delete/permanently/<int:project_id>/", DeleteProjectPermanentlyView.as_view(), name="delete-project-permanently"),
+
+    # PROJECT TEAM
+    path("team/get/<int:project_id>/", RetrieveProjectTeamMembersView.as_view(), name="retrieve-project-team-members"),
+    path("team/remove/<int:project_member_id>/", RemoveProjectTeamMemberView.as_view(), name="remove-project-team-member"),
+
+    # PROJECT MEMBERSHIP ROUTES
+    path("membership/request/<int:project_id>/", RequestProjectMembershipView.as_view(), name="request-project-membership"),
+    path("membership/request/get/<int:project_id>/", RetrieveProjectMembershipRequestsView.as_view(), name="retrieve-project-membership-requests"),
+    path("membership/request/accept/<int:project_request_id>/", AcceptProjectMembershipView.as_view(), name="accept-project-membership-request"),
+    path("membership/request/reject/<int:project_request_id>/", RejectProjectMembershipView.as_view(), name="reject-project-membership-request"),
+    path("membership/request/delete/<int:project_request_id>/", DeleteProjectMembershipRequestView.as_view(), name="delete-project-membership-request"),
+
+    # PROJECT INVITATION ROUTES
+    path("invitation/invite/", InviteResearchAssistantView.as_view(), name="invite-research-assistant"),
+    path("invitation/get/<int:project_id>/", RetrieveProjectInvitationsView.as_view(), name="retrieve-project-invitations"),
+    path("invitation/accept/<int:project_invite_id>/", AcceptProjectInvitationView.as_view(), name="accept-project-invitation"),
+    path("invitation/decline/<int:project_invite_id>/", DeclineProjectInvitationView.as_view(), name="reject-project-invitation"),
+    path("invitation/delete/<int:project_invite_id>/", DeleteProjectInvitationView.as_view(), name="delete-project-invitation"),
 
     # MILESTONE ROUTES
     path("milestone/add/", AddMilestoneView.as_view(), name="add-milestone"),
