633bbba7dc7c2a33b862cc9aeb6a87caa549eece
Richard Quayson <richard.quayson@ashesi.edu.gh>
2023-12-12 04:22:00 +0000

    react files

--- Frontend/arrm-frontend/src/index.js
+++ Frontend/arrm-frontend/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Background from './components/background';
-import LoginPage from './components/login';
-import Request from './components/request';
-import Reset from './components/reset';
-import Success from './components/success';
-import Dashboard from './components/dashboard';
-import BackgroundPage from './pages/backgroundpage';
+// import LoginPage from './components/login';
+// import Request from './components/request';
+// import Reset from './components/reset';
+// import Success from './components/success';
+// import Dashboard from './components/dashboard';
+// import BackgroundPage from './pages/backgroundpage';
 import TheLogin from './pages/loginpage';
 import TheRequest from './pages/requestpage';
 import TheReset from './pages/resetpage';
@@ -35,12 +35,12 @@ ReactDOM.render(
   <Router>
     <Routes>
       <Route exact path="/" element={<Background />} />
-      <Route path="/login" element={<LoginPage />} />
+      {/* <Route path="/login" element={<LoginPage />} />
       <Route path="/request" element={<Request />} />
       <Route path="/reset" element={<Reset />} />
       <Route path="/success" element={<Success />} /> 
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/backgroundpage" element={<BackgroundPage />} />
+      <Route path="/backgroundpage" element={<BackgroundPage />} /> */}
       <Route path="/loginpage" element={<TheLogin />} />
       <Route path="/requestpage" element={<TheRequest />} />
       <Route path="/resetpage" element={<TheReset />} />
